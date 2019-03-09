da1d64a26b6cd61f3c25929c6d30608ea09e15d3
aly-samesystem <ahmed.aly@samesystem.com>
2019-03-09 14:52:47 +0200

    first commit

---
+++ Automation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Automation
+//
+//  Created by Ahmed Aly on 3/9/19.
+//  Copyright © 2019 Ahmed Aly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
